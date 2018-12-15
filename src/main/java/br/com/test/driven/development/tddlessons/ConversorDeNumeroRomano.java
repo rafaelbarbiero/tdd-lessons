f75dda2f8d1d0282d3d6cc6b7479bcfaff6e6e1b
Rafael Barbiero <rafael.barbiero@hotmail.com>
2018-12-15 17:12:35 -0200

    Capitulo 3.2 - O primeiro teste (usando map)

--- src/main/java/br/com/test/driven/development/tddlessons/ConversorDeNumeroRomano.java
+++ src/main/java/br/com/test/driven/development/tddlessons/ConversorDeNumeroRomano.java
@@ -1,11 +1,22 @@
 package br.com.test.driven.development.tddlessons;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConversorDeNumeroRomano {
+    private static Map<String, Integer> tabela = new HashMap<String, Integer>() {{
+        put("I", 1);
+        put("II", 2);
+        put("V", 5);
+        put("X", 10);
+        put("L", 50);
+        put("C", 100);
+        put("D", 500);
+        put("M", 1000);
+    }};
 
     public int converte(String numeroEmRomano) {
-        if (numeroEmRomano.equals("I")) return 1;
-        else if (numeroEmRomano.equals("V")) return 5;
-
-        return 0;
+        return tabela.get(numeroEmRomano);
     }
+
 }
