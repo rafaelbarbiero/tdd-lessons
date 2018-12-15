6d6e9580b434c2839e57160799d6501bd2c981d0
Rafael Barbiero <rafael.barbiero@hotmail.com>
2018-12-15 18:00:50 -0200

    Capitulo 4.2 - Implementando da maneira mais simples possível (Baby steps generalizando - com falha)

--- src/test/java/br/com/test/driven/development/tddlessons/CalculadoraDeSalarioTest.java
+++ src/test/java/br/com/test/driven/development/tddlessons/CalculadoraDeSalarioTest.java
@@ -5,15 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculadoraDeSalarioTest {
-
     @Test
     public void
-    deveCalcularSalarioParaDesenvolvedoresComSalarioAbaixoDoLimite() {
+    deveCalcularSalarioParaDesenvolvedoresComSalarioAcimaDoLimite() {
         CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
-        Funcionario desenvolvedor = new Funcionario
-                ("Mauricio", 1500.0, Cargo.DESENVOLVEDOR);
+        Funcionario desenvolvedor = new Funcionario("Mauricio", 4000.0, Cargo.DESENVOLVEDOR);
         double salario = calculadora.calculaSalario(desenvolvedor);
-        assertEquals(1500.0 * 0.9, salario, 0.00001);
+        assertEquals(4000.0 * 0.8, salario, 0.00001);
     }
-
 }
