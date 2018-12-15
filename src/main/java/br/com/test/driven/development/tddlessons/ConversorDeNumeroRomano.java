22c71085b890cdd8a0f7ef3e15f928fee91b4c4f
Rafael Barbiero <rafael.barbiero@hotmail.com>
2018-12-15 17:08:18 -0200

    Capitulo 3.2 - O primeiro teste (com sucesso)

--- src/main/java/br/com/test/driven/development/tddlessons/ConversorDeNumeroRomano.java
+++ src/main/java/br/com/test/driven/development/tddlessons/ConversorDeNumeroRomano.java
@@ -3,6 +3,9 @@ package br.com.test.driven.development.tddlessons;
 public class ConversorDeNumeroRomano {
 
     public int converte(String numeroEmRomano) {
+        if (numeroEmRomano.equals("I")) return 1;
+        else if (numeroEmRomano.equals("V")) return 5;
+
         return 0;
     }
 }
--- src/test/java/br/com/test/driven/development/tddlessons/ConversorDeNumeroRomanoTest.java
+++ src/test/java/br/com/test/driven/development/tddlessons/ConversorDeNumeroRomanoTest.java
@@ -19,6 +19,4 @@ public class ConversorDeNumeroRomanoTest {
         int numero = romano.converte("V");
         assertEquals(5, numero);
     }
-
-
 }
