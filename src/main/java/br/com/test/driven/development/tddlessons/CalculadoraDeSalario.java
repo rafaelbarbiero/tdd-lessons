708bdb6474e3267fe6c1b4fe4b11022d9b01797b
Rafael Barbiero <rafael.barbiero@hotmail.com>
2018-12-15 17:44:45 -0200

    Capitulo 4.2 - Implementando da maneira mais simples possível (passando)

--- src/main/java/br/com/test/driven/development/tddlessons/CalculadoraDeSalario.java
+++ src/main/java/br/com/test/driven/development/tddlessons/CalculadoraDeSalario.java
@@ -1,8 +1,12 @@
 package br.com.test.driven.development.tddlessons;
 
 public class CalculadoraDeSalario {
+
     public double calculaSalario(Funcionario funcionario) {
-        if(funcionario.getSalario() > 3000) return 3200.0;
-        return 1350.0;
+        if(funcionario.getCargo().equals(Cargo.DESENVOLVEDOR)) {
+            if(funcionario.getSalario() > 3000) return 3200.0;
+            return 1350.0;
+        }
+        return 425.0;
     }
 }
