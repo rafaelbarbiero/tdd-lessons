1f48fb1e12a8c7eaa037ff1fd2937fc5c502f421
Rafael Barbiero <rafael.barbiero@hotmail.com>
2018-12-15 17:56:33 -0200

    Capitulo 4.2 - Implementando da maneira mais simples possível (Baby steps generalizando)

--- src/main/java/br/com/test/driven/development/tddlessons/CalculadoraDeSalario.java
+++ src/main/java/br/com/test/driven/development/tddlessons/CalculadoraDeSalario.java
@@ -3,6 +3,6 @@ package br.com.test.driven.development.tddlessons;
 public class CalculadoraDeSalario {
 
     public double calculaSalario(Funcionario funcionario) {
-        return 1500 * 0.9;
+        return funcionario.getSalario() * 0.9;
     }
 }
