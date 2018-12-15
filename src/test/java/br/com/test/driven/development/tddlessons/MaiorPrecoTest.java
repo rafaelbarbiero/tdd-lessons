905c0013af7b3622494887c0e40eb4103c4b0227
Rafael Barbiero <rafael.barbiero@hotmail.com>
2018-12-15 18:08:32 -0200

    Capitulo 5.1 - O problema do carrinho de compras (com falha)

--- src/test/java/br/com/test/driven/development/tddlessons/MaiorPrecoTest.java
+++ src/test/java/br/com/test/driven/development/tddlessons/MaiorPrecoTest.java
@@ -13,4 +13,15 @@ public class MaiorPrecoTest {
         double valor = algoritmo.encontra(carrinho);
         assertEquals(0.0, valor, 0.0001);
     }
+
+    @Test
+    public void deveRetornarValorDoItemSeCarrinhoCom1Elemento() {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        carrinho.adiciona(new Item("Geladeira", 1, 900.0));
+        MaiorPreco algoritmo = new MaiorPreco();
+        double valor = algoritmo.encontra(carrinho);
+        assertEquals(900.0, valor, 0.0001);
+    }
+
+
 }
