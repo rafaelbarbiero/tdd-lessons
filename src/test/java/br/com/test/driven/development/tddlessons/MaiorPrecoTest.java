19cc038aa8d7e3e62c7e6140000d7837d7b6264b
Rafael Barbiero <rafael.barbiero@hotmail.com>
2018-12-15 18:18:58 -0200

    Capitulo 6 - Qualidade no código de teste

--- src/test/java/br/com/test/driven/development/tddlessons/MaiorPrecoTest.java
+++ src/test/java/br/com/test/driven/development/tddlessons/MaiorPrecoTest.java
@@ -1,21 +1,27 @@
 package br.com.test.driven.development.tddlessons;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class MaiorPrecoTest {
 
+    CarrinhoDeCompras carrinho;
+
+    @Before
+    public void inicializa() {
+        this.carrinho = new CarrinhoDeCompras();
+    }
+
     @Test
     public void deveRetornarZeroSeCarrinhoVazio() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
         double valor = carrinho.maiorValor();
         assertEquals(0.0, valor, 0.0001);
     }
 
     @Test
     public void deveRetornarValorDoItemSeCarrinhoCom1Elemento() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
         carrinho.adiciona(new Item("Geladeira", 1, 900.0));
         double valor = carrinho.maiorValor();
         assertEquals(900.0, valor, 0.0001);
@@ -23,7 +29,6 @@ public class MaiorPrecoTest {
 
     @Test
     public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
         carrinho.adiciona(new Item("Geladeira", 1, 900.0));
         carrinho.adiciona(new Item("Fogão", 1, 1500.0));
         carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
